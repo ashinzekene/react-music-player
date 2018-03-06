54a9ee8490189145dec3349ed52eb3c51e119280
ashinzekene <ashinzekene@gmail.com>
2018-03-06 14:56:59 +0100

    created views

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import AddSongs from './components/AddSongs';
-import MyAppBar from './components/MyAppBar'
-import SongList from './components/SongList'
-import NowPlaying from './components/NowPlaying'
+import MainView from './views/MainView';
 import { togglePlaying, playSong } from "./actions";
 
 injectTapEventPlugin()
@@ -36,80 +33,11 @@ class App extends React.Component {
     super(props)
     this.state = {}
   }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.playState !== this.props.playState) {
-      if (!nextProps.playState.playing) {
-        // PAUSE
-        this.audioPlayer.pause()
-      } else if (nextProps.playState.songId === this.props.playState.songId) {
-        // RESUME
-        this.audioPlayer.play()
-      } else {
-        // Start playing
-        this.playSong(nextProps.playState.songId)
-      }
-
-    }
-  }
-
-  playSong = (id) => {
-    const { songs } = this.props
-    if (songs[id]) {
-      console.log("Will play song", id)
-      let fileSrc = URL.createObjectURL(songs[id])
-      this.audioPlayer.src = fileSrc
-      this.audioPlayer.play()
-    }
-  }
-
-  songEnded = () => {
-    const { songs, playState, shuffle } = this.props
-    // No shuffle
-    if (shuffle === 0) {
-      URL.revokeObjectURL(songs[playState.songId])
-      playState.songId < songs.length && this.props.playSong(playState.songId + 1)
-    }
-    // Shuffle 1
-    else if (shuffle === 1) {
-      this.props.playSong(playState.id)
-    }
-    // shuffle all
-    else {
-      this.playNext()
-    }
-  }
-  
-  playNext = () => {
-    const { songs, playState, shuffle } = this.props    
-    URL.revokeObjectURL(songs[playState.songId])
-    let nextSongId = (playState.songId + 1) % songs.length
-    this.props.playSong(nextSongId)
-  }
-  
-  playPrevious = () => {
-    const { songs, playState, shuffle } = this.props
-    URL.revokeObjectURL(songs[playState.songId])
-    let nextSongId =  playState.songId === 0 ? songs.length - 1: playState.songId + 1//(playState.songId + ((songs.length - 1)) % songs.length
-    this.props.playSong(nextSongId)
-  }
-
-  updateTime = () => {
-    const currentTime = 100 * this.audioPlayer.currentTime / this.audioPlayer.duration
-    this.setState({ currentTime })
-  }
-
   render() {
-    let { currentTime } = this.state
-    let { songs, playState } = this.props
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div>
-          <MyAppBar />
-          <SongList songs={songs} />
-          <AddSongs />
-          <audio controls hidden onTimeUpdate={this.updateTime} onEnded={this.songEnded} ref={(audio) => this.audioPlayer = audio} />
-          <NowPlaying togglePlaying={ this.props.toggle } playState={playState} playNext={ this.playNext } song={songs[playState.songId]} currentTime={currentTime} />
+          <MainView/>
         </div>
       </MuiThemeProvider>
     )
