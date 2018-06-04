353901b42e66025adbf361c1b11b7eeef5f5b097
Ashinzekene <ashinzekene@gmail.com>
2018-06-04 16:53:07 +0100

    open source

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 
  [CHECK OUT THE WEB APP](https://ashinzekene.github.io/react-music-player)
 
+  ![React-music-player](image.png)
+
+## CONTRIBUTING
+
+Feel free to contribute to the repo. Make sure you configure eslint, or run lint before submitting pull requests
+### TECH STACK
+- React
+- Redux
+
 ## TODO LIST
 
 1. Play Next Automatically ✅
@@ -13,10 +22,11 @@
 1. A Page for currently playing song ✅
 1. Host on GitHub ✅
 1. Repeat ✅
+1. Seek progressbar on nowPlayingPage ✅
 1. Let playing song show as playing
 1. Add Icons to sidebar
 1. Show Time
-1. Seek progressbar on nowPlayingPage
+1. Fix linting
 1. Shuffle
 1. Search
 1. Playlists
--- src/index.js
+++ src/index.js
@@ -5,28 +5,28 @@ import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
-import reducers from './reducers'
-import { loggerMiddleware } from "./middleware";
-import { saveState, getState } from './store/localStore'
+import reducers from './reducers';
+import { loggerMiddleware } from './middleware';
+import { saveState, getState } from './store/localStore';
 // import createStoreObserver from  'redux-store-observer'
 
-getState().then(localState => {
-  let store
-  if (process.env.NODE_ENV === "development") {
-    store = createStore(reducers, localState, applyMiddleware(loggerMiddleware))
+getState().then((localState) => {
+  let store;
+  if (process.env.NODE_ENV === 'development') {
+    store = createStore(reducers, localState, applyMiddleware(loggerMiddleware));
   } else {
-    store = createStore(reducers, localState)
+    store = createStore(reducers, localState);
   }
   store.subscribe(() => {
     saveState({
-      songs: store.getState().songs
-    })
-  })
+      songs: store.getState().songs,
+    });
+  });
   ReactDOM.render(
     <Provider store={store}>
       <App />
+      {/* eslint-disable-next-line */}
     </Provider>, document.getElementById('root'));
-  
-})
+});
 registerServiceWorker();
 
