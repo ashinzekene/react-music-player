cee934dc0345b478d047e30b16de15eff8090601
ashinzekene <ashinzekene@gmail.com>
2018-11-17 07:55:10 +0100

    Updates

---
+++ precache-manifest.d1bc97409b944f329bee1cc91e14b5f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac77fe0b4ca62a24eada",
+    "url": "/react-music-player/static/js/runtime~main.ac77fe0b.js"
+  },
+  {
+    "revision": "33e3683b1e69bbee15c4",
+    "url": "/react-music-player/static/js/main.33e3683b.chunk.js"
+  },
+  {
+    "revision": "81f1ea0fed709af2ab6a",
+    "url": "/react-music-player/static/js/1.81f1ea0f.chunk.js"
+  },
+  {
+    "revision": "33e3683b1e69bbee15c4",
+    "url": "/react-music-player/static/css/main.676ecb85.chunk.css"
+  },
+  {
+    "revision": "517cc066b2ba411f7d59b30ec85f9570",
+    "url": "/react-music-player/index.html"
+  }
+];
